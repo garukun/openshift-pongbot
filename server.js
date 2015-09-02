f877d52809c13cba903f7f58fcb57e58828186f7
Steve Jiang <steve.jiang@vungle.com>
2015-09-01 21:37:59 -0700

    Add express IP.

--- server.js
+++ server.js
@@ -7,16 +7,18 @@
 'use strict';
 
 var mongoose = require('mongoose');
-var mongoUri = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME
-    + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@'
-    + process.env.OPENSHIFT_MONGODB_DB_HOST  + ':'
-    + process.env.OPENSHIFT_MONGODB_DB_PORT;
+var mongoUri = process.env.OPENSHIFT_MONGODB_DB_URL
+    || 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME
+        + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@'
+        + process.env.OPENSHIFT_MONGODB_DB_HOST  + ':'
+        + process.env.OPENSHIFT_MONGODB_DB_PORT
+    || 'mongodb://localhost:27018';
 mongoose.connect(mongoUri);
 
 var app = require('./lib/app').instance();
 
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Listening on port', port);  
+app.listen(port, ip, function() {
+  console.log('Listening on port', port);
 });
