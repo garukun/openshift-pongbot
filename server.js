54b6f1f6ac41a24d60d8a57e02538401d81af8be
Steve Jiang <steve.jiang@vungle.com>
2015-09-01 19:29:59 -0700

    Fix mongo url.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
 
 var mongoose = require('mongoose');
 var mongoUri = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME
-    + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD
+    + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@'
     + process.env.OPENSHIFT_MONGODB_DB_HOST  + ':'
     + process.env.OPENSHIFT_MONGODB_DB_PORT;
 mongoose.connect(mongoUri);
