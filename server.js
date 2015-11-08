5061f4d42c6e86ba0d7a4934ece7b2c32202498c
Ahmed Mokhtar <ahmed.mokhtar@vungle.com>
2015-11-08 12:48:45 +0000

    Improving styling
    correcting indentations

--- server.js
+++ server.js
@@ -8,10 +8,10 @@
 
 var mongoose = require('mongoose');
 var mongoUri = process.env.DEV_PONG_MONGODB_DB_URL || process.env.OPENSHIFT_MONGODB_DB_URL
-    || 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME
-    + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@'
-    + process.env.OPENSHIFT_MONGODB_DB_HOST + ':'
-    + process.env.OPENSHIFT_MONGODB_DB_PORT;
+  || 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME
+  + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@'
+  + process.env.OPENSHIFT_MONGODB_DB_HOST + ':'
+  + process.env.OPENSHIFT_MONGODB_DB_PORT;
 mongoose.connect(mongoUri);
 
 var app = require('./lib/app').instance();
@@ -19,5 +19,5 @@ var app = require('./lib/app').instance();
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
 app.listen(port, ip, function () {
-    console.log('Listening on port', port);
+  console.log('Listening on port', port);
 });
